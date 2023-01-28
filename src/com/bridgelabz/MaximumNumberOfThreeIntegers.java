83584d88e310e86d6b2106e7c6e179450a426190
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-28 19:09:01 +0530

    Added: Maximum number from Three integers

--- src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
+++ src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
@@ -1,10 +1,18 @@
 package com.bridgelabz;
-
-public class MaximumNumberOfThreeIntegers<I> {
-
+import java.lang.Integer;
+public class MaximumNumberOfThreeIntegers<P extends Comparable<P>> {
+    public P maximumNumber(P number1, P number2, P number3){
+        P maximum = number1;
+        if (number2.compareTo(maximum) > 0){
+            maximum = number2;
+        }else if (number3.compareTo(maximum) > 0){
+            maximum = number2;
+        }
+        return maximum;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Test for maximum number");
-
+        MaximumNumberOfThreeIntegers<Integer> maximum1 = new MaximumNumberOfThreeIntegers<>();
+        System.out.println("the maximum number is " + maximum1.maximumNumber(100, 4, 54));
     }
-
 }

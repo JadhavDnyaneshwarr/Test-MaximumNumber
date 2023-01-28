1758e05386ac4d319e9029bd452c85ac92217089
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-28 15:40:51 +0530

    welcome to Test for Maximum number program

---
+++ src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class MaximumNumberOfThreeIntegers<I> {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test for maximum number");
+
+    }
+
+}

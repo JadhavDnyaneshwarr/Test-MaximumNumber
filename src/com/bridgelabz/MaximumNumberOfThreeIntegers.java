77f39350d22cdb31b70a1da727e85fde1d9fb9cf
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-28 19:40:21 +0530

    Added: Maximum number from Three Float Numbers

--- src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
+++ src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
@@ -15,5 +15,8 @@ public class MaximumNumberOfThreeIntegers<P extends Comparable<P>> {
         MaximumNumberOfThreeIntegers<Integer> maximum1 = new MaximumNumberOfThreeIntegers<>();
         System.out.println("the maximum number is " + maximum1.maximumNumber(100, 14, 54));
         System.out.println("the maximum number is " + maximum1.maximumNumber(100, 114, 54));
+        System.out.println("the maximum number is " + maximum1.maximumNumber(100, 114, 154));
+        MaximumNumberOfThreeIntegers<Float> maximum2 = new MaximumNumberOfThreeIntegers<>();
+        System.out.println("the maximum number is " + maximum2.maximumNumber(101F, 269F, 51F));
     }
 }

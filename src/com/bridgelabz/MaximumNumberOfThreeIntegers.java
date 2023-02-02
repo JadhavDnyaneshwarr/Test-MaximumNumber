e4b7568d056ccd846de06d6fcef9c15871526dae
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-02 18:11:35 +0530

    Refactor: Maximum Number of Generic Instance Varible In Static Method

--- src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
+++ src/com/bridgelabz/MaximumNumberOfThreeIntegers.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 import java.lang.Integer;
-public class MaximumNumberOfThreeIntegers {
-    public static  <P extends Comparable<P>> P maximumNumber(P number1, P number2, P number3){
+public class MaximumNumberOfThreeIntegers<P extends Comparable<P>> {
+    P number1;
+    P number2;
+    P number3;
+
+    public MaximumNumberOfThreeIntegers() {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.number3 = number3;
+    }
+
+    public static <P extends Comparable<P>> P maximumNumber(P number1, P number2, P number3){
         P maximum = number1;
         if (number2.compareTo(maximum) > 0){
             maximum = number2;
@@ -22,16 +32,25 @@ public class MaximumNumberOfThreeIntegers {
         }
         return maximum;
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Test for maximum number");
-        System.out.println("the maximum number is " + maximumNumber(100, 14, 54));
-        System.out.println("the maximum number is " + maximumNumber(100, 114, 54));
-        System.out.println("the maximum number is " + maximumNumber(100, 114, 154));
+        MaximumNumberOfThreeIntegers<Integer> maximumInteger = new MaximumNumberOfThreeIntegers<>();
+        maximumInteger.number1 = 100;
+        maximumInteger.number2 = 109;
+        maximumInteger.number3 = 190;
+        System.out.println("the maximum number is " + maximumInteger.maximumNumber(maximumInteger.number1, maximumInteger.number2, maximumInteger.number3));
         System.out.println();
-        System.out.println("the maximum number is " + maximumNumber(101F, 69F, 51F));
-        System.out.println("the maximum number is " + maximumNumber(101F, 169F, 51F));
-        System.out.println("the maximum number is " + maximumNumber(101F, 169F, 511F));
+        MaximumNumberOfThreeIntegers<Float> maximumFloat = new MaximumNumberOfThreeIntegers<>();
+        maximumFloat.number1 = 130F;
+        maximumFloat.number2 = 184F;
+        maximumFloat.number3 = 160F;
+        System.out.println("the maximum number is " + maximumFloat.maximumNumber(maximumInteger.number1, maximumInteger.number2, maximumInteger.number3));
         System.out.println();
-        System.out.println("the maximum string is " + maximumNumber("Apple", "Peach", "Banana"));
+        MaximumNumberOfThreeIntegers<String> maximumString = new MaximumNumberOfThreeIntegers<>();
+        maximumString.number1 = "Apple";
+        maximumString.number2 = "Peach";
+        maximumString.number3 = "Banana";
+        System.out.println("the maximum number is " + maximumString.maximumNumber(maximumInteger.number1, maximumInteger.number2, maximumInteger.number3));
     }
 }
